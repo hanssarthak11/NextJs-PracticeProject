a0cc774513b132a7d820dc3cf20909189861d143
Sarthak <hanssarthak3@gmail.com>
2024-07-13 14:27:39 +0530

    Commit

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -25,7 +25,7 @@ export default async function RevenueChart() {
         Recent Revenue
       </h2>
       <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
+        <div className="sm:grid-cols-13 mt-0 grid grid-cols-13 items-end gap-1 rounded-md bg-white p-4 md:gap-4">
           {/* y-axis */}
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
@@ -37,7 +37,7 @@ export default async function RevenueChart() {
           </div>
 
           {revenue.map((month) => (
-            <div key={month.month} className="flex flex-col items-center gap-2">
+            <div key={month.month} className="flex flex-col items-center gap-1">
               {/* bars */}
               <div
                 className="w-full rounded-md bg-blue-300"
